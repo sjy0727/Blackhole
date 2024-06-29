a1aced0bd09a7eec594d3630f96cd7354c23b9d7
sjy0727 <981963252@qq.com>
2024-06-29 23:59:08 +0800

    Update GLDebugMessageCallback.h

--- src/GLDebugMessageCallback.h
+++ src/GLDebugMessageCallback.h
@@ -2,10 +2,12 @@
 // Feel free to use this in any way.
 #pragma once
 
+#include <cstring>
 #include <iostream>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
-    GLenum severity, GLsizei length,
-    const GLchar* msg, const void* data);
+                                     GLenum severity, GLsizei length,
+                                     const GLchar *msg, const void *data);
